b77817fcf17d1b6a55774ab3a95b4c396990fb9d
Dan <danporeda@yahoo.com>
2024-04-04 00:48:33 -0400

    installed bulma, used cards, app complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-
-      <ProfileCard title="alexa" handle="@alexa69" />
-      <ProfileCard title="Cotana" handle="@cuntyC" />
-      <ProfileCard title="Siri" handle="@SuckySiri" />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard 
+                image={AlexaImage} 
+                title="alexa" 
+                handle="@alexa69" 
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard 
+                image={CortanaImage} 
+                title="Cotana" 
+                handle="@cuntyC" 
+                description="Cortana is a basic bitch by Microsoft"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard 
+                image={SiriImage} 
+                title="Siri" 
+                handle="@SuckySiri" 
+                description="Siri can suck it"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
